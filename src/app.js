537d17baf7cabd2d4237638a9923ff4b6a4147a4
Habibov97 <najaff.habibov@gmail.com>
2026-03-17 18:02:23 +0400

    InitialSetup-DoggyStickers-Project-BE

---
+++ src/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const config = require('./config');
+const morgan = require('morgan');
+
+const app = express();
+
+app.use(cors());
+app.use(morgan('dev'));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Welcome to DoggyStickers app!!!' });
+});
+
+app.listen(config.port, () => {
+  console.log(`Listening to http://localhost:${config.port}`);
+});
